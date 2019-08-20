b0e8f0b455b94ab9863151d0d4332bb7772f770c
vemonet <vincent.emonet@gmail.com>
2019-08-20 22:13:02 +0200

    add steps and feature file for SPARQL tests

---
+++ features/steps/steps-sparql.py
@@ -0,0 +1,56 @@
+from behave import *
+from SPARQLWrapper import SPARQLWrapper, JSON, XML, CSV, TSV
+import numpy as np
+
+"""
+Given the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
+When we get the entity "http://identifiers.org/drugbank:DB00001"
+Then its property "bl:name" should be "Lepirudin"
+"""
+
+
+@given('the dataset "{graphUri}"')
+def step_impl(context, graphUri):
+    context.graphUri = graphUri
+
+@when('we get the entity "{entityUri}"')
+def step_impl(context, entityUri):
+    context.entityUri = entityUri
+
+# Run SPARQL to get the value of the required property
+@then('its property "{predicate}" should be "{value}"')
+def step_impl(context, predicate, value):
+
+    sparql = SPARQLWrapper("http://graphdb.dumontierlab.com/repositories/ncats-red-kg")
+    query = """
+    PREFIX bl: <http://w3id.org/biolink/vocab/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX dctypes: <http://purl.org/dc/dcmitype/>
+    PREFIX idot: <http://identifiers.org/idot/>
+    PREFIX dcat: <http://www.w3.org/ns/dcat#>
+    PREFIX void: <http://rdfs.org/ns/void#>
+    PREFIX void-ext: <http://ldf.fi/void-ext#>
+    SELECT ?propertyValue
+    WHERE {
+        GRAPH <%s> {
+            <%s> %s ?propertyValue .
+        }
+    }
+    """
+    sparql.setQuery(query % (context.graphUri, context.entityUri, predicate))
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
+    print(results["results"]["bindings"])
+    
+    # This loop doesn't work. Even if print shows that results["results"]["bindings"] is a list
+    # Tried to cast it to Array using numpy np.asarray(myList), but still not working
+    # If anyone can find why Python can't do a for loop on an array I would be interested.
+    # See SPARQL example here: https://rdflib.github.io/sparqlwrapper/
+    for result in np.asarray(results["results"]["bindings"]):
+        print("we don't get into this loop!")
+    # for i in range(len(results["results"]["bindings"])):
+
+    # We get the first answer directly from array index because loop won't work
+    assert value == results["results"]["bindings"][0]["propertyValue"]["value"]
