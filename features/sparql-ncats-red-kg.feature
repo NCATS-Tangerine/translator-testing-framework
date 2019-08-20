79f610fee0a90067243d187e3554d860e4c00323
vemonet <vincent.emonet@gmail.com>
2019-08-21 00:30:30 +0200

    add new SPARQL test to directly write the query in the feature file

--- features/sparql-ncats-red-kg.feature
+++ features/sparql-ncats-red-kg.feature
@@ -4,3 +4,15 @@ Feature: Test ncats-red-kg triplestore answers from given SPARQL queries to dete
     Given the entity "http://identifiers.org/drugbank:DB00001" in the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
     When we ask for the property "bl:name"
     Then we get the answer "Lepirudin"
+
+  Scenario: Check for DrugBank drug ID using a SPARQL query
+    Given the sparql endpoint "http://graphdb.dumontierlab.com/repositories/ncats-red-kg"
+    When we run the sparql query:
+    """
+      SELECT * WHERE {
+          GRAPH <https://w3id.org/data2services/graph/biolink/drugbank> {
+              <http://identifiers.org/drugbank:DB00001> bl:id ?id .
+          }
+      }
+    """
+    Then the sparql result "id" should be "DB00001"
--- features/steps/steps-sparql.py
+++ features/steps/steps-sparql.py
@@ -2,12 +2,73 @@ from behave import *
 from SPARQLWrapper import SPARQLWrapper, JSON, XML, CSV, TSV
 import numpy as np
 
-"""
-Given the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
-When we get the entity "http://identifiers.org/drugbank:DB00001"
-Then its property "bl:name" should be "Lepirudin"
+prefixes = """
+PREFIX bl: <http://w3id.org/biolink/vocab/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dctypes: <http://purl.org/dc/dcmitype/>
+PREFIX idot: <http://identifiers.org/idot/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX void: <http://rdfs.org/ns/void#>
+PREFIX void-ext: <http://ldf.fi/void-ext#>
 """
 
+def run_sparql_query(query_string):
+    sparql = SPARQLWrapper("http://graphdb.dumontierlab.com/repositories/ncats-red-kg")
+    sparql.setQuery(prefixes + query_string)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results["results"]["bindings"]
+
+
+
+"""
+Given the sparql endpoint "http://graphdb.dumontierlab.com/repositories/ncats-red-kg"
+When we run the sparql query:
+"" "
+    SELECT ?propertyValue WHERE {
+        GRAPH <https://w3id.org/data2services/graph/biolink/drugbank> {
+            <http://identifiers.org/drugbank:DB00001> bl:name ?name .
+        }
+    }
+"" "
+Then the sparql result "name" should be "Lepirudin"
+"""
+
+@given('the sparql endpoint "{sparqlEndpoint}"')
+def step_impl(context, sparqlEndpoint):
+    context.sparqlEndpoint = sparqlEndpoint
+
+
+@when('we run the sparql query')
+def step_impl(context):
+    context.query = context.text
+
+
+# Run SPARQL to get the value of the required property
+@then('the sparql result "{predicate}" should be "{value}"')
+def step_impl(context, predicate, value):
+    results = run_sparql_query(context.query)
+    
+    # This loop doesn't work. Even if print shows that results["results"]["bindings"] is a list
+    # Tried to cast it to Array using numpy np.asarray(myList), but still not working
+    # If anyone can find why Python can't do a for loop on an array I would be interested.
+    # See SPARQL example here: https://rdflib.github.io/sparqlwrapper/
+    for result in np.asarray(results):
+        print("we don't get into this loop!")
+    # for i in range(len(results["results"]["bindings"])):
+
+    # We get the first answer directly from array index because loop won't work
+    assert value == results[0][predicate]["value"]
+
+
+
+
+"""
+Given the entity "http://identifiers.org/drugbank:DB00001" in the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
+When we ask for the property "bl:name"
+Then we get the answer "Lepirudin"
+"""
 
 @given('the entity "{entityUri}" in the dataset "{graphUri}"')
 def step_impl(context, entityUri, graphUri):
@@ -46,14 +107,8 @@ def step_impl(context, value):
 
     results = sparql.query().convert()
     print(results["results"]["bindings"])
-    
-    # This loop doesn't work. Even if print shows that results["results"]["bindings"] is a list
-    # Tried to cast it to Array using numpy np.asarray(myList), but still not working
-    # If anyone can find why Python can't do a for loop on an array I would be interested.
-    # See SPARQL example here: https://rdflib.github.io/sparqlwrapper/
     for result in np.asarray(results["results"]["bindings"]):
         print("we don't get into this loop!")
-    # for i in range(len(results["results"]["bindings"])):
 
     # We get the first answer directly from array index because loop won't work
     assert value == results["results"]["bindings"][0]["propertyValue"]["value"]
